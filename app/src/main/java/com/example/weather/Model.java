ef842cc4b96e8a859ca6585038763e44a510e4fe
Mohamed <mnafe44@gmail.com>
2025-08-01 21:26:11 +0300

    first commit

---
+++ app/src/main/java/com/example/weather/Model.java
@@ -0,0 +1,19 @@
+package com.example.weather;
+import java.util.List;
+public class Model {
+    public Main main;
+    public List<Weather> weather;
+    public Wind wind;
+    public String name;
+    public class Main {
+        public float temp;
+        public int humidity;
+    }
+    public class Weather {
+        public String description;
+        public String icon;
+    }
+    public class Wind {
+        public float speed;
+    }
+}
